afc89acb8b565a75c6a747b9d1bb240e06277db5
mohilio324 <mehdicherbi944@gmail.com>
2026-04-25 18:20:20 +0100

    note:modifcations for testing purposes dont touch 5

--- new/vite.config.js
+++ new/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-
     plugins: [react()],
+    // Set base to '/' for Vercel (GitHub Pages usually required a project name here)
+    base: '/',
+    build: {
+        // This ensures your build files go into the 'dist' folder which Vercel expects
+        outDir: 'dist',
+    }
 })
